ef4b73de9bd26f62946f877fb76c045cc37cd4b2
ingressboy <81222444+ingressboy@users.noreply.github.com>
2021-11-26 13:35:24 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,37 +2,52 @@ local discordia = require('discordia')
 local client = discordia.Client()
 
 client:on('ready', function()
-	print('Logged in as '.. client.user.username)
+    local channel = client:getChannel('913329624824557600')
+    local connection = channel:join()
+    connection:playFFmpeg('music.mp3')
+    print('Logged in as '.. client.user.username)
 end)
 
 client:on('messageCreate', function(message)
-	if message.content == '=ping' then
-		message.channel:send('Pong!')
-	end
+    if message.content == '=ping' then
+        message.channel:send('Pong!')
+    end
 end)
 
 client:on('messageCreate', function(message)
-	if message.content == '=help' then
-		message.channel:send('Hilfe :')
-	end
+    if message.content == '=help' then
+        message.channel:send('Hilfe :\n')
+    end
 end)
 
 client:on('messageCreate', function(message)
-	if message.content == '=version' then
-		message.channel:send('v0.3')
-	end
+    if message.content == '=version' then
+        message.channel:send('v0.4')
+    end
 end)
 
 client:on('messageCreate', function(message)
-	if message.content == '=twitter' then
-		message.channel:send('https://twitter.com/ingressboy')
-	end
+    if message.content == '=twitter' then
+        message.channel:send('https://twitter.com/ingressboy')
+    end
 end)
 
 client:on('messageCreate', function(message)
-	if message.content == '=github' then
-		message.channel:send('https://github.com/ingressboy/Missao-Lua')
-	end
+    if message.content == '=github' then
+        message.channel:send('https://github.com/ingressboy/Missao-Lua')
+    end
 end)
 
-client:run('Bot INSERT_TOKEN_HERE')
+client:on('messageCreate', function(message)
+    if message.content == 'Moin' then
+        message.channel:send('Moin!')
+    end
+end)
+
+client:on('messageCreate', function(message)
+    if message.content == 'Hallo' then
+        message.channel:send('Hallo!')
+    end
+end)
+
+client:run('Bot OTA5NDM1OTU2MDAyNjk3MzA5.YZEQPA.973HCEOZ5hgnL8Jd6kuLbGbx0pc')
