92ab48360cbef188f5ba301399814a4ebeabb5f0
ingressboy <81222444+ingressboy@users.noreply.github.com>
2021-12-01 19:07:10 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local discordia = require('discordia')
 local client = discordia.Client()
 
 client:on('ready', function()
-    local channel = client:getChannel('913329624824557600')
+    local channel = client:getChannel('--')
     local connection = channel:join()
     connection:playFFmpeg('music.mp3')
     print('Logged in as '.. client.user.username)
